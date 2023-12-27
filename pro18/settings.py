0528e7f86549845c087e2d4a80a1cf79816fdca9
srikanthpasam <pasam01134@gmail.com>
2023-12-27 07:19:13 +0530

    thanks

--- pro18/settings.py
+++ pro18/settings.py
@@ -138,6 +138,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sekharguna691@gmail.com'
-EMAIL_HOST_PASSWORD = 'tqjmbwlmpmbjmodn'
-
+EMAIL_HOST_USER = 'pasam01134@gmail.com'
+EMAIL_HOST_PASSWORD = 'tdlp tvrf xxhj auto'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+EMAIL_USE_SSL = False
